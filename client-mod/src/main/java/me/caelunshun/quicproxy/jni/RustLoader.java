3e3bc90b5658b055d8c700839232d1bd56319bbb
Caelum van Ispelen <caelum12321@gmail.com>
2024-02-10 19:02:42 -0700

    Draft loading JNI libraries dependent on OS

--- client-mod/src/main/java/me/caelunshun/quicproxy/jni/RustLoader.java
+++ client-mod/src/main/java/me/caelunshun/quicproxy/jni/RustLoader.java
@@ -6,8 +6,24 @@ import java.io.*;
 
 public class RustLoader {
     public static void loadNativeLibraries() throws IOException {
-        try (InputStream stream = RustLoader.class.getClassLoader().getResourceAsStream("minecraft_quic_proxy_jni.dll")) {
-            File tempFile = File.createTempFile("quic-proxy", "jni-library.dll");
+        String os = System.getProperty("os.name").toLowerCase();
+        String resourceName;
+        String fileExtension;
+        if (os.contains("win")) {
+            resourceName = "minecraft_quic_proxy_jni";
+            fileExtension = "dll";
+        } else if (os.contains("nix") || os.contains("nux") || os.contains("aix")) {
+            resourceName = "libminecraft_quic_proxy_jni";
+            fileExtension = "so";
+        } else if (os.contains("mac")) {
+            resourceName = "libminecraft_quic_proxy_jni";
+            fileExtension = "dylib";
+        } else {
+            throw new RuntimeException("unsupported OS: " + os);
+        }
+
+        try (InputStream stream = RustLoader.class.getClassLoader().getResourceAsStream(resourceName + "." + fileExtension)) {
+            File tempFile = File.createTempFile("quic-proxy", "jni-library." + fileExtension);
             tempFile.deleteOnExit();
             try (OutputStream outputStream = new FileOutputStream(tempFile)) {
                 IOUtils.copy(stream, outputStream);
